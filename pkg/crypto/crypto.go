9777ff87f33014022e127d18f139eab39e002542
Samuel Bines <samuelbines@gmail.com>
2026-04-30 18:45:11 +0100

    shared crypto

---
+++ pkg/crypto/crypto.go
@@ -0,0 +1,36 @@
+package secrets
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"io"
+)
+
+func Encrypt(secretKey, value string) (string, error) {
+	key := deriveKey(secretKey)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, []byte(value), nil)
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+func deriveKey(secretKey string) []byte {
+	sum := sha256.Sum256([]byte(secretKey))
+	return sum[:]
+}
